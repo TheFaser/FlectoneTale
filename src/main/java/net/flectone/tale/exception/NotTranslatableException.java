635e72d0edbafe90e32064505878c26cd59abc6d
TheFaser <artem.mochalin11@gmail.com>
2026-01-15 12:18:42 +0500

    refactor: remove unused exception classes

--- src/main/java/net/flectone/tale/exception/NotTranslatableException.java
+++
@@ -1,9 +0,0 @@
-package net.flectone.tale.exception;
-
-public class NotTranslatableException extends RuntimeException {
-
-    public NotTranslatableException() {
-        super("Component is not translatable or has an unknown translation key");
-    }
-
-}
--- src/main/java/net/flectone/tale/exception/SchedulerTaskException.java
+++
@@ -1,4 +0,0 @@
-package net.flectone.tale.exception;
-
-public class SchedulerTaskException extends Exception {
-}
